14f74392335d4a4b5d6e006c63565115be897e42
woodjess3 <woodjess3@myvuw.ecs.nz>
2022-03-24 14:20:10 +1300

    Add CNN code

---
+++ code/cnn/Main.py
@@ -0,0 +1,61 @@
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+from .data import load, prepare, normalize, encode_labels
+from .cnn import get_model  
+from .plot import show_confusion_matrix
+
+folder = "data/matlab/"
+datasets = ["Fish.mat","Part.mat"]
+dataset = datasets[0]
+print("Chosen: %s" % dataset)
+mat = load(dataset,folder=folder)
+X,y = prepare(mat)
+X,_ = normalize(X,X)
+y, le = encode_labels(y)
+
+class_names = le.inverse_transform(np.unique(y)).tolist()
+print(class_names)
+num_classes = len(np.unique(y))
+
+# Convert labels to one-hot encoding.
+y = tf.keras.utils.to_categorical(y, num_classes=num_classes)
+
+# Batch axis (source: https://bit.ly/3yAF1J7)
+X = np.expand_dims(X, axis=-1)
+
+input_shape = (X.shape[1],X.shape[2])
+batch_size = 32
+
+print(f"Input shape: {input_shape}")
+
+X_train, X_val, y_train, y_val = train_test_split(X,y)
+
+train_ds = tf.data.Dataset.from_tensor_slices((X_train,y_train)).shuffle(1000).batch(batch_size)
+val_ds = tf.data.Dataset.from_tensor_slices((X_val,y_val)).shuffle(1000).batch(batch_size)
+
+model = get_model(num_classes=num_classes)
+history = model.fit(train_ds, validation_data=val_ds, epochs=20)
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('cnn/assets/accuracy.png')
+plt.show()
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('cnn/assets/loss.png')
+plt.show()
+
+show_confusion_matrix(X_val, y_val, model, labels=class_names)
