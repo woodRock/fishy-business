a84299b85837d97bf341525c352594e505bbeabc
woodjess3 <woodjess3@myvuw.ecs.nz>
2022-05-05 13:19:46 +1200

    Main + Data - documentation

---
+++ code/gp/Main.py
@@ -0,0 +1,15 @@
+"""
+Main - Main.py
+==============
+
+This is the main routine for a Genetric Progamming model. 
+
+DEAP Github - https://github.com/DEAP/deap
+GP Tutorial - https://machinelearningmastery.com/simple-genetic-algorithm-from-scratch-in-python/
+"""
+
+# TODO
+# [ ] Complete the tutorial.
+# [ ] Apply this to GC fish classification task. 
+# [ ] Try basic DEAP algorithm. 
+# [ ] Implement elitism.
