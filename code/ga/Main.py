018f48289cf16ef447f5189baffd035e19ff6bd4
woodrock <j.r.h.wood98@gmail.com>
2022-08-05 23:56:37 +1200

    plot.py - a utility module for GA

--- code/ga/Main.py
+++ code/ga/Main.py
@@ -2,19 +2,19 @@
 Main - Main.py
 ==============
 
-This is the main routine for a Genetric Algorithm (GA) model. 
+This is the main routine for a Genetric Algorithm (GA) model.
 
-Using GA we solve (1) onemax, and (2) 2nd-order polynomial equations. 
+Using GA we solve (1) onemax, and (2) 2nd-order polynomial equations.
 GA implements evaluation, selection and reproduction on a bitstring representations of the solution.
 Evaluation measures the performance of the solution by the objective function.
-We perform tournament selection to decide which individuals are kepts. 
+We perform tournament selection to decide which individuals are kepts.
 The reproduction is done by crossover and mutation.
 
-References: 
-	1.  Simple Genetic Algorithm From Scratch in Python, Jason Brownlee, 
-		Machine Learning Mastery https://machinelearningmastery.com/simple-genetic-algorithm-from-scratch-in-python/
+References:
+    1.  Simple Genetic Algorithm From Scratch in Python, Jason Brownlee,
+        Machine Learning Mastery https://machinelearningmastery.com/simple-genetic-algorithm-from-scratch-in-python/
 
-TODO: 
+TODO:
 DEAP Github - https://github.com/DEAP/deap
 """
 
@@ -26,6 +26,7 @@ DEAP Github - https://github.com/DEAP/deap
 
 from numpy.random import randint
 from numpy.random import rand
+from .plot import plot_error
 
 
 def selection(pop, scores, k=3):
@@ -33,12 +34,12 @@ def selection(pop, scores, k=3):
     Tournament selection.
 
     Args:
-            pop: Population.
-            scores: Scores.
-            k: Number of participants.
+                    pop: Population.
+                    scores: Scores.
+                    k: Number of participants.
 
     Returns:
-            Selection individual.
+                    Selection individual.
     """
     # First random selection.
     selection_ix = randint(len(pop))
@@ -54,8 +55,8 @@ def mutation(bitstring, r_mut):
     Mutation.
 
     Args:
-            individual: Individual.
-            r_mut: Mutation rate.
+                    individual: Individual.
+                    r_mut: Mutation rate.
     """
     for i, _ in enumerate(bitstring):
         # Check for a mutation.
@@ -69,12 +70,12 @@ def crossover(p1, p2, r_cross):
     Crossover.
 
     Args:
-            p1: Parent 1.
-            p2: Parent 2.
-            r_cross: Crossover rate.
+                    p1: Parent 1.
+                    p2: Parent 2.
+                    r_cross: Crossover rate.
 
-    Returns: 
-            Children.
+    Returns:
+                    Children.
     """
     # Children are copies of parents by default.
     c1, c2 = p1.copy(), p2.copy()
@@ -92,26 +93,26 @@ def onemax(x):
     """
     A onexmax objective function (boring).
 
-    With the OneMax problem you are searching for a bitstring where all values are filled with ones. 
+    With the OneMax problem you are searching for a bitstring where all values are filled with ones.
 
     Args:
-            x: An individual.
+                    x: An individual.
 
     Returns:
-            The objective function value.
+                    The objective function value.
     """
     return -sum(x)
 
 
 def objective(x):
     """
-    2nd order polynomial objective funciton (interesting). 
+    2nd order polynomial objective funciton (interesting).
 
     Args:
-            x: An individual.
+                    x: An individual.
 
     Returns:
-            The objective function value.
+                    The objective function value.
     """
     return x[0]**2.0 + x[1]**2.0
 
@@ -121,12 +122,12 @@ def decode(bounds, n_bits, bitstring):
     Decodes a bitstring into a real number.
 
     Args:
-            bounds: Bounds.
-            n_bits: Number of bits.
-            bitstring: Bitstring.
+                    bounds: Bounds.
+                    n_bits: Number of bits.
+                    bitstring: Bitstring.
 
-    Returns: 
-            Decoded number.
+    Returns:
+                    Decoded number.
     """
     decoded = list()
     largest = 2**n_bits
@@ -151,35 +152,40 @@ def genetic_algorithm(objective, bounds, n_bits, n_iter, n_pop, r_cross, r_mut):
     Genetic algorithm.
 
     Args:
-            objective: Objective function.
-            n_bits: Number of bits.
-            n_iter: Number of iterations.
-            n_pop: Number of population.
-            r_cross: Crossover rate.
-            r_mut: Mutation rate.
+                    objective: Objective function.
+                    n_bits: Number of bits.
+                    n_iter: Number of iterations.
+                    n_pop: Number of population.
+                    r_cross: Crossover rate.
+                    r_mut: Mutation rate.
 
     Returns:
-            Best solution.
+                    Best solution.
     """
+    errors = []
     # initial population of random bitstring
     pop = [randint(0, 2, n_bits*len(bounds)).tolist() for _ in range(n_pop)]
     # keep track of best solution
     best, best_eval = 0, objective(pop[0])
+
     # enumerate generations
     for gen in range(n_iter):
         # Decode population.
         decoded = [decode(bounds, n_bits, p) for p in pop]
         # evaluate all candidates in the population
         scores = [objective(d) for d in decoded]
+
         # check for new best solution
         for i in range(n_pop):
             if scores[i] < best_eval:
                 best, best_eval = pop[i], scores[i]
                 print(f">{gen}, new best f({decoded[i]}) = {scores[i]}")
+                errors.append(best_eval)
         # select parents
         selected = [selection(pop, scores) for _ in range(n_pop)]
         # create the next generation
         children = []
+
         for i in range(0, n_pop, 2):
             # get selected parents in pairs
             p1, p2 = selected[i], selected[i+1]
@@ -191,6 +197,8 @@ def genetic_algorithm(objective, bounds, n_bits, n_iter, n_pop, r_cross, r_mut):
                 children.append(c)
         # replace population
         pop = children
+
+    plot_error(errors)
     return [best, best_eval]
 
 
