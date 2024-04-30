68712b39580f60f916fd2de690bb395416df2df4
woodRock <j.r.h.wood98@gmail.com>
2024-05-01 02:10:06 +1200

    Finish coding

--- code/evotorch/problem.py
+++ code/evotorch/problem.py
@@ -9,8 +9,12 @@ class ProgramSynthesisProblem(Problem):
         self,
         unary_ops: Iterable,
         binary_ops: Iterable,
-        inputs: Iterable,
-        outputs: Iterable,
+        X_train: Iterable,
+        y_train: Iterable,
+        X_val: Iterable,
+        y_val: Iterable,
+        X_test: Iterable,
+        y_test: Iterable,
         program_length: int,
         pass_means_terminate: bool = True,
         device: Optional[Union[str, torch.device]] = None,
@@ -23,12 +27,25 @@ class ProgramSynthesisProblem(Problem):
             device = torch.device(device)
 
         self._program_length = int(program_length)
-        self._inputs = torch.as_tensor(inputs, dtype=torch.float32, device=device)
-        self._outputs = torch.as_tensor(outputs, dtype=torch.float32, device=device)
+                
+        self._X_train = torch.as_tensor(X_train, dtype=torch.float32, device=device)
+        self._y_train = torch.as_tensor(y_train, dtype=torch.float32, device=device)
+        self._X_val = torch.as_tensor(X_val, dtype=torch.float32, device=device)
+        self._y_val = torch.as_tensor(y_val, dtype=torch.float32, device=device)
+        self._X_test = torch.as_tensor(X_test, dtype=torch.float32, device=device)
+        self._y_test = torch.as_tensor(y_test, dtype=torch.float32, device=device)
 
-        self._input_batch_size, self._input_size = self._inputs.shape
-        [output_batch_size] = self._outputs.shape
-        assert output_batch_size == self._input_batch_size
+        self._X_train_batch_size, self._X_train_size = self._X_train.shape
+        [y_train_batch_size] = self._y_train.shape
+        assert y_train_batch_size == self._X_train_batch_size
+
+        self._X_val_batch_size, self._X_val_size = self._X_val.shape
+        [y_val_batch_size] = self._y_val.shape
+        assert y_val_batch_size == self._X_val_batch_size
+
+        self._X_test_batch_size, self._X_test_size = self._X_test.shape
+        [y_test_batch_size] = self._y_test.shape
+        assert y_test_batch_size == self._X_test_batch_size
 
         self._unary_ops = list(unary_ops)
         self._binary_ops = list(binary_ops)
@@ -48,17 +65,29 @@ class ProgramSynthesisProblem(Problem):
             store_solution_stats=True,
         )
 
+    def _set_interpreter(self, num_programs, input_size, input_batch_size) -> None:
+        self._interpreter = InterpreterWithInputBatch(
+            max_stack_length=self._program_length,
+            program_batch_size=num_programs,
+            input_size=input_size,
+            input_batch_size=input_batch_size,
+            unary_ops=self._unary_ops,
+            binary_ops=self._binary_ops,
+            pass_means_terminate=self._pass_means_terminate,
+            device=self._X_train.device,
+         )
+
     def _get_interpreter(self, num_programs: int) -> InterpreterWithInputBatch:
         if (self._interpreter is None) or (num_programs > self._interpreter.program_batch_size):
-            self._interpreter = InterpreterWithInputBatch(
+             self._interpreter = InterpreterWithInputBatch(
                 max_stack_length=self._program_length,
                 program_batch_size=num_programs,
-                input_size=self._input_size,
-                input_batch_size=self._input_batch_size,
+                input_size=self._X_train_size,
+                input_batch_size=self._X_train_batch_size,
                 unary_ops=self._unary_ops,
                 binary_ops=self._binary_ops,
                 pass_means_terminate=self._pass_means_terminate,
-                device=self._inputs.device,
+                device=self._X_train.device,
             )
         return self._interpreter
 
@@ -76,7 +105,12 @@ class ProgramSynthesisProblem(Problem):
         else:
             programs = batch.values
 
-        batch.set_evals(interpreter.compute_balanced_accuracy(programs, self._inputs, self._outputs)[:num_programs])
+        # Run the problem over the training, validation, and test datasets.
+        batch.set_evals(interpreter.compute_balanced_accuracy(programs, self._X_train, self._y_train)[:num_programs])
+        self._set_interpreter(self._X_val_size, self._X_val_size, self._X_val_batch_size)
+        batch.set_evals(interpreter.compute_balanced_accuracy(programs, self._X_val, self._y_val)[:num_programs])
+        self._set_interpreter(self._X_test_size, self._X_test_size, self._X_test_batch_size)
+        batch.set_evals(interpreter.compute_balanced_accuracy(programs, self._X_test, self._y_test)[:num_programs])
 
     @property
     def instructions(self) -> list:
