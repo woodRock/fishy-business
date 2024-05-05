32d621e494a81f733d7f22ca284ab58b6a975aa6
woodrock <j.r.h.wood98@gmail.com>
2024-05-05 16:03:12 +1200

    Tidy code

--- code/gp/data.py
+++ code/gp/data.py
@@ -31,7 +31,8 @@ def load_dataset(
     y = []
     # Remove the quality control samples.
     data = data[~data['m/z'].str.contains('QC')]
-        # Exclude cross-species samples from the dataset.
+    
+    # Exclude cross-species samples from the dataset.
     if dataset == "species" or dataset == "part" or dataset == "oil":
         data = data[~data['m/z'].str.contains('HM')]
     
