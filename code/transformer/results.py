6b77c14a1615049bdb3ed2aa077c25981f133db4
woodRock <j.r.h.wood98@gmail.com>
2024-05-13 18:37:18 +1200

    Run experiments on the grid + add results.py file to gather results from logs

---
+++ code/transformer/results.py
@@ -0,0 +1,61 @@
+# Results - results.py
+# Date: 2024-05-13
+# Author: Jesse Wood
+# 
+# This script reads the results from a file and collects
+# information about the mean and standard deviation.
+
+import os 
+import torch 
+
+if __name__ == "__main__":
+    # Set verbose to true for debugging.
+    verbose = True  
+    # Select the dataset to process results for.
+    datasets = ["species", "part", "oil", "cross-species"]
+    dataset = datasets[2] # Oil
+    # Path to the logging folder.
+    folder = os.path.join("logs",dataset,"nsp")
+    # Lists to collect the results with.
+    train_accs = []
+    val_accs = [] 
+    test_accs = []
+    # For each experiment in a batch of 30 independent runs.
+    for i in range(1,10+1):
+        file_name = f"run_{i}.log"
+        if verbose:
+            print(f"file_name: {file_name}")
+        file_path = os.path.join(folder, file_name)
+        with open(file_path) as f:
+            content = f.readlines()
+            # Extract the train, validation and test accuracy.
+            print(f"len(content): {len(content)}")
+            # The training accuracy is on line 342.
+            train_acc: float = float(content[342].split(sep=' ')[-1])
+            # The validation accuracy is on line 244.
+            val_acc: float = float(content[344].split(sep=' ')[-1])
+            # The test accuracy is on line 346.
+            test_acc: float = float(content[346].split(sep=' ')[-1])
+            # Append the accuracy to an array.
+            train_accs.append(train_acc)
+            val_accs.append(val_acc)
+            test_accs.append(test_acc)
+            
+            # Verbose output for debugging purposes
+            if verbose: 
+                print(f"Train: {train_acc}")
+                print(f"Validation: {val_acc}")
+                print(f"Test: {test_acc}")
+
+    # Convert to tensors.
+    train_accs = torch.tensor(train_accs)
+    val_accs = torch.tensor(val_accs)
+    test_accs = torch.tensor(test_accs)
+
+    # Mean and standard deviation for each results array.
+    mean, std = torch.mean(train_accs), torch.std(train_accs)
+    print(f"Training: mean: {mean} +\- {std}")
+    mean, std = torch.mean(val_accs), torch.std(val_accs)
+    print(f"Validation: mean: {mean} +\- {std}")
+    mean, std = torch.mean(test_accs), torch.std(test_accs)
+    print(f"Test: mean: {mean} +\- {std}")
