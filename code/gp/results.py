fb30ef58cd8a4e7a91be7ef499d7a0455f913f5f
woodRock <j.r.h.wood98@gmail.com>
2024-05-15 17:19:16 +1200

    Fix results.py - count lines from the end

--- code/gp/results.py
+++ code/gp/results.py
@@ -4,23 +4,43 @@
 # 
 # This script reads the results from a file and collects
 # information about the mean and standard deviation.
-
+import argparse
 import os 
 import torch 
 
 if __name__ == "__main__":
+    # Handle the command line arguments for the script.
+    parser = argparse.ArgumentParser(
+                    prog='Transformer: results',
+                    description='A transformer for fish species classification.',
+                    epilog='Implemented in pytorch and written in python.')
+    parser.add_argument('-d', '--dataset', type=str, default="species",
+                        help="The fish species or part dataset. Defaults to species")
+    parser.add_argument('-f', '--folder', type=str, default="tmp",
+                        help="The folder to get the results from. Defaults to tmp")
+    args = vars(parser.parse_args())
+
     # Set verbose to true for debugging.
-    verbose = False  
+    verbose = False 
+
     # Select the dataset to process results for.
     datasets = ["species", "part", "oil", "cross-species"]
-    dataset = datasets[0] # Part
+    dataset = args['dataset'] # Part
+    if dataset not in datasets:
+        raise ValueError(f"Invalid dataset specified: {dataset}")
+    
     print(f"dataset: {dataset}")
     # Path to the logging folder.
-    folder = os.path.join("logs",dataset,"tmp_3")
+    folder = os.path.join("logs",dataset,args['folder'])
     # Lists to collect the results in.
     train_accs = []
     val_accs = [] 
     test_accs = []
+
+    # Skip the print tree statements.
+    skip = {'species': 2, 'part': 6, 'oil': 2, 'cross-species': 3}
+    skip_amount = skip[dataset]
+
     # For each experiment in a batch of 30 independent runs.
     for i in range(1,30+1):
         file_name = f"run_{i}.log"
@@ -30,12 +50,12 @@ if __name__ == "__main__":
         with open(file_path) as f:
             content = f.readlines()
             # Extract the train, validation and test accuracy.
-            # The training accuracy is on line 411.
-            train_acc: float = float(content[411].split(sep=' ')[2])
-            # The validation accuracy is on line 412.
-            val_acc: float = float(content[412].split(sep=' ')[2])
-            # The test accuracy is on line 413.
-            test_acc: float = float(content[413].split(sep=' ')[2])
+            # The training accuracy is on the 10th to last line.
+            train_acc: float = float(content[-8 - skip_amount].split(sep=' ')[2])
+            # The validation accuracy is on the 9th to last line.
+            val_acc: float = float(content[-7 - skip_amount].split(sep=' ')[2])
+            # The test accuracy is on the 8th to last line.
+            test_acc: float = float(content[-6 - skip_amount].split(sep=' ')[2])
             # Append the accuracy to an array.
             train_accs.append(train_acc)
             val_accs.append(val_acc)
