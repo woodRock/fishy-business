ec5a55f6f7fd87f24ae69c068fa7e2919a9abe45
woodrock <j.r.h.wood98@gmail.com>
2024-05-25 22:42:40 +1200

    Revert run.sh script to use task spooler (ts)

--- code/transformer/run.sh
+++ code/transformer/run.sh
@@ -17,17 +17,17 @@ then
 fi 
 
 # The server has 3 GPUs to use in parallel.
-# ts -S 3
+ts -S 3
 
 for i in {1..10}; 
 do 
     # Run the experiments using the ts command.
-#     ts -G 1 python3 main.py \
-#       --next-spectra-prediction \
+    ts -G 1 python3 main.py \
         python3 main.py \
         --dataset "${DATASET}" --output "logs/${DATASET}/${NAME}/run" --run "$i" \
         --data-augmentation \
 	--masked-spectra-modelling \
+         --next-spectra-prediction \
 	--dropout 0.2 --label-smoothing 0.1 --early-stopping 5 \
         --epochs 100 --learning-rate "1E-5";
 done
