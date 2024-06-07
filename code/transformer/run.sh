167b54d57fe9819a7650dcebfd8be5559c833ca9
woodrock <j.r.h.wood98@gmail.com>
2024-06-07 21:10:57 +1200

    Change back to encoder-decoder architecture

--- code/transformer/run.sh
+++ code/transformer/run.sh
@@ -7,7 +7,7 @@
 DATASET="oil"; 
 # DATASET="species";
 # Directory to save results to.
-NAME="tmp_03";
+NAME="tmp_04";
 
 # If the directory does not already exsit.
 if [ ! -d "logs/${DATASET}/${NAME}" ];
@@ -19,13 +19,13 @@ fi
 # The server has 3 GPUs to use in parallel.
 ts -S 3
 
-for i in {1..30}; 
+for i in {1..10}; 
 do 
     # Run the experiments using the ts command.
     ts -G 1 python3 main.py \
         --dataset "${DATASET}" --output "logs/${DATASET}/${NAME}/run" --run "$i" \
-        --data-augmentation \
-	--masked-spectra-modelling \
-	--dropout 0.2 --label-smoothing 0.1 --early-stopping 10 \
+	    --data-augmentation \
+        --masked-spectra-modelling \
+	    --dropout 0.2 --label-smoothing 0.1 --early-stopping 10 \
         --epochs 100 --learning-rate "1E-5";
 done
