77d9fcf0048bb3e3cf5ffd2134b9f89b646cd40e
woodRock <j.r.h.wood98@gmail.com>
2024-07-23 20:06:21 +1200

    Tidy PSO

--- code/pso/main.py
+++ code/pso/main.py
@@ -4,69 +4,71 @@ import time
 from util import preprocess_dataset
 from pso import PSO
 
-# Example usage
-if __name__ == "__main__":
+def parse_arguments():
     parser = argparse.ArgumentParser(
-                    prog='Particle Swarm Optimization (PSO)',
-                    description='A PSO for fish species classification.',
-                    epilog='Implemented in pytorch and written in python.')
-    parser.add_argument('-d', '--dataset', type=str, default="species",
-                         help="The fish species or part dataset. Defaults to species")
-    parser.add_argument('-f', '--file-path', type=str, default="cnn_checkpoint",
-                        help="Filepath to store the model checkpoints to. Defaults to transformer_checkpoint")
-    parser.add_argument('-r', '--run', type=int, default=0)
-    parser.add_argument('-o', '--output', type=str, default=f"logs/results")
-
-    # Preprocessing
-    parser.add_argument('-da', '--data-augmentation',
-                    action='store_true', default=False,
-                    help="Flag to perform data augmentation. Defaults to False.")  
-    # Regularization
-    parser.add_argument('-es', '--early-stopping', type=int, default=10,
-                        help='Early stopping patience. To disable early stopping set to the number of epochs. Defaults to 5.')
-    parser.add_argument('-ls', '--label-smoothing', type=float, default=0.1,
-                        help="The alpha value for label smoothing. 1 is a uniform distribution, 0 is no label smoothing. Defaults to 0.1")
-    # Hyperparameters
-    parser.add_argument('-g', '--generations', type=int, default=100,
-                        help="The number of epochs to train the model for.")
-    parser.add_argument('-lr', '--learning-rate', type=float, default=1E-3,
-                        help="The learning rate for the model. Defaults to 1E-3.")
-    parser.add_argument('-bs', '--batch-size', type=int, default=64,
-                        help='Batch size for the DataLoader. Defaults to 64.')
+        prog='Genetic Algorithm',
+        description='A genetic algorithm (GA) for fish species classification.',
+        epilog='Implemented in deap and written in Python.')
     
-    args = vars(parser.parse_args())
+    parser.add_argument('-f', '--file-path', type=str, default="checkpoints/embedded-gp.pth", 
+                        help="The filepath to store the checkpoints. Defaults to checkpoints/embedded-gp.pth")
+    parser.add_argument('-d', '--dataset', type=str, default="species", 
+                        help="The fish species or part dataset. Defaults to species.")
+    parser.add_argument('-l', '--load', action='store_true',
+                        help="To load a checkpoint from a file. Defaults to false")
+    parser.add_argument('-r', '--run', type=int, default=0,
+                        help="The number for the run, this affects the random seed. Defaults to 0")
+    parser.add_argument('-o', '--output', type=str, default="logs/results",
+                        help="Partial filepath for the output logging. Defaults to 'logs/results'.")
+    parser.add_argument('-bs', '--batch-size', type=int, default=64,
+                        help="The batch size for the data loaders. Defaults to 64.")
+    parser.add_argument('-p', '--population', type=int, default=100,
+                        help="The number of individuals in the population. Defaults to 100.")
+    parser.add_argument('-b', '--beta', type=int, default=-1,
+                        help="Specify beta * num_features as population size. Defaults to -1.")
+    parser.add_argument('-g', '--generations', type=int, default=100,
+                        help="The number of generations, or epochs, to train for. Defaults to 100.")
+    parser.add_argument('-mx', '--mutation-rate', type=float, default=0.2,
+                        help="The probability of a mutation operation occurring. Defaults to 0.2")
+    parser.add_argument('-cx', '--crossover-rate', type=float, default=0.8,
+                        help="The probability of a crossover operation occurring. Defaults to 0.8")
+    parser.add_argument('-e', '--elitism', type=float, default=0.1,
+                        help="The ratio of elitists to be kept each generation.")
+    
+    return parser.parse_args()
 
-    # Logging output to a file.
+def setup_logging(args):
     logger = logging.getLogger(__name__)
-    # Run argument for numbered log files.
-    output = f"{args['output']}_{args['run']}.log"
-    # Filemode is write, so it clears the file, then appends output.
+    output = f"{args.output}_{args.run}.log"
     logging.basicConfig(filename=output, level=logging.INFO, filemode='w')
-    file_path = f"checkpoints/{args['file_path']}_{args['run']}.pth"
+    return logger
 
-    dataset = args['dataset']
-    is_data_augmentation = args['data_augmentation']
-    generations = args['generations']
-    learning_rate = args['learning_rate']
-    batch_size = args['batch_size']
-    label_smoothing = args['label_smoothing']
+
+def main():
+    args = parse_arguments()
+    logger = setup_logging(args)
+
+    n_features = 1023
+    n_classes_per_dataset = {"species": 2, "part": 6, "oil": 7, "cross-species": 3}
+
+    if args.dataset not in n_classes_per_dataset:
+        raise ValueError(f"Invalid dataset: {args.dataset} not in {n_classes_per_dataset.keys()}")
+    
+    n_classes = n_classes_per_dataset[args.dataset]
 
     train_loader, val_loader = preprocess_dataset(
-        dataset=dataset,
-        batch_size=batch_size,
-        is_data_augmentation=is_data_augmentation,
+        dataset=args.dataset,
+        batch_size=args.batch_size,
+        is_data_augmentation=True,
         is_pre_train=False
     )
-    
-    # Get the number of features and classes from the data
-    X_sample, y_sample = next(iter(train_loader))
-    n_features = X_sample.shape[1]
-    n_classes = y_sample.shape[1]  # Assuming one-hot encoded labels
+
+    population = args.beta * n_features if args.beta != -1 else args.population
     
     # Initialize and train PSO classifier
     model = PSO(
-        n_particles=500, 
-        n_iterations=generations, 
+        n_particles=population, 
+        n_iterations=args.generations, 
         c1=0.4, c2=0.4, 
         w_start=0.9, w_end=0.4,
         n_classes=n_classes,
@@ -81,6 +83,11 @@ if __name__ == "__main__":
     # Evaluate the model on the training and validation DataLoaders.
     train_accuracy = model.evaluate(train_loader)
     val_accuracy = model.evaluate(val_loader)
+
+    # Display the final train and test accuracy.
     score_str = f"Training Accuracy: {train_accuracy:.4f} Validation Accuracy: {val_accuracy:.4f}"
     logger.info(score_str)
     print(f"{score_str}")
+
+if __name__ == "__main__":
+    main()
