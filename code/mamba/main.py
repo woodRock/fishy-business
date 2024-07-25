dea63a470a2496eb50993a78ab37fe5ef7a2812e
woodRock <j.r.h.wood98@gmail.com>
2024-07-25 20:46:29 +1200

    Mamba

---
+++ code/mamba/main.py
@@ -0,0 +1,115 @@
+import argparse
+import logging 
+import torch
+import torch.nn as nn 
+import torch.optim as optim
+from mamba import Mamba
+from util import preprocess_dataset
+from train import train_model
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+                    prog='Convolutional Neural Network (CNN)',
+                    description='A CNN for fish species classification.',
+                    epilog='Implemented in pytorch and written in python.')
+    parser.add_argument('-d', '--dataset', type=str, default="species",
+                         help="The fish species or part dataset. Defaults to species")
+    parser.add_argument('-f', '--file-path', type=str, default="cnn_checkpoint",
+                        help="Filepath to store the model checkpoints to. Defaults to transformer_checkpoint")
+    parser.add_argument('-r', '--run', type=int, default=0)
+    parser.add_argument('-o', '--output', type=str, default=f"logs/results")
+
+    # Preprocessing
+    parser.add_argument('-da', '--data-augmentation',
+                    action='store_true', default=False,
+                    help="Flag to perform data augmentation. Defaults to False.")  
+    # Pre-training
+    parser.add_argument('-msm', '--masked-spectra-modelling',
+                    action='store_true', default=False,
+                    help="Flag to perform masked spectra modelling. Defaults to False.")  
+    parser.add_argument('-nsp', '--next-spectra-prediction',
+                    action='store_true', default=False,
+                    help="Flag to perform next spectra prediction. Defaults to False.") 
+    # Regularization
+    parser.add_argument('-es', '--early-stopping', type=int, default=10,
+                        help='Early stopping patience. To disable early stopping set to the number of epochs. Defaults to 5.')
+    parser.add_argument('-do', '--dropout', type=float, default=0.2,
+                        help="Probability of dropout. Defaults to 0.2")
+    parser.add_argument('-ls', '--label-smoothing', type=float, default=0.1,
+                        help="The alpha value for label smoothing. 1 is a uniform distribution, 0 is no label smoothing. Defaults to 0.1")
+    # Hyperparameters
+    parser.add_argument('-e', '--epochs', type=int, default=100,
+                        help="The number of epochs to train the model for.")
+    parser.add_argument('-lr', '--learning-rate', type=float, default=1E-3,
+                        help="The learning rate for the model. Defaults to 1E-3.")
+    parser.add_argument('-bs', '--batch-size', type=int, default=64,
+                        help='Batch size for the DataLoader. Defaults to 64.')
+    parser.add_argument('-hd', '--hidden-dimension', type=int, default=1023,
+                        help='The dimensions of the hidden dimension. Defaults to 1023.')
+    parser.add_argument('-sd', '--state-dimension', type=int, default=16,
+                        help='The dimensions of the state dimension. Defaults to 16.')
+    parser.add_argument('-dc', '--conv-dimension', type=int, default=4,
+                        help='The dimensions of the convlutional dimension. Defaults to 4.')
+    parser.add_argument('-ex', '--expand', type=int, default=2,
+                        help='The number of dimensions to expand. Defaults to 2.')
+    parser.add_argument('-nl', '--num-layers', type=int, default=1,
+                        help='The dimensions of the hidden dimension. Defaults to 1.')
+
+    return parser.parse_args()
+
+
+def setup_logging(args):
+    logger = logging.getLogger(__name__)
+    output = f"{args.output}_{args.run}.log"
+    logging.basicConfig(filename=output, level=logging.INFO, filemode='w')
+    return logger
+
+
+def main():
+    args = parse_arguments()
+    logger = setup_logging(args)
+
+    n_features = 1023
+    n_classes_per_dataset = {"species": 2, "part": 6, "oil": 7, "cross-species": 3}
+
+    if args.dataset not in n_classes_per_dataset:
+        raise ValueError(f"Invalid dataset: {args.dataset} not in {n_classes_per_dataset.keys()}")
+    
+    n_classes = n_classes_per_dataset[args.dataset]
+
+    # Load the dataset.
+    train_loader, val_loader, _, _, _ = preprocess_dataset(
+        args.dataset, 
+        args.data_augmentation, 
+        batch_size=args.batch_size,
+        is_pre_train=False
+    )   
+
+    # Example usage
+    model = Mamba(
+        d_model=args.hidden_dimension,
+        d_state=args.state_dimension,
+        d_conv=args.conv_dimension,
+        expand=args.expand,
+        depth=args.num_layers,
+        n_classes=n_classes
+    )
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    criterion = nn.CrossEntropyLoss(label_smoothing=args.label_smoothing)
+    optimizer = optim.AdamW(model.parameters(), lr=args.learning_rate)
+
+    train_model(
+        model = model, 
+        train_loader = train_loader,
+        val_loader = val_loader,
+        criterion = criterion,
+        optimizer = optimizer,
+        num_epochs = args.epochs,
+        patience = args.early_stopping
+    )
+    
+
+if __name__ == "__main__":
+    main()
---
+++ code/mamba/mamba.py
@@ -0,0 +1,124 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MambaBlock(nn.Module):
+    def __init__(self, 
+        d_model: int, 
+        d_state: int, 
+        d_conv: int, 
+        expand: int,
+    ) -> None:
+        """ MambaBlock
+        
+        Args: 
+            d_model (int): the input dimension.
+            d_state (int): the state dimension.
+            d_conv (int): the convolutional kernel size.
+            expand (int): the expansion factor.
+        """
+        super().__init__()
+        self.d_model = d_model
+        self.d_state = d_state
+        self.d_conv = d_conv
+        self.expand = expand
+        
+        self.in_proj = nn.Linear(d_model, expand * d_model)
+        self.conv = nn.Conv1d(expand * d_model, expand * d_model, kernel_size=d_conv, padding=d_conv-1, groups=expand * d_model)
+        self.activation = nn.SiLU()
+        
+        self.x_proj = nn.Linear(expand * d_model, d_state + d_model)
+        self.dt_proj = nn.Linear(expand * d_model, d_state)
+        
+        self.out_proj = nn.Linear(expand * d_model, d_model)
+        
+        # Add a projection for us
+        self.us_proj = nn.Linear(d_state, d_model)
+        
+    def forward(self, 
+        x: torch.Tensor
+    ) -> torch.Tensor:
+        """ Forward pass
+
+        Args: 
+            x (torch.Tensor): the input tensor.
+
+        Returns:
+            x (torch.Tensor): the output tensor.
+        """
+        # This will give you the desired shape of [64,1,16] -> [64,100,16].
+        B, L, D = x.shape
+        
+        x_in = self.in_proj(x)
+        x_conv = x_in.transpose(1,2)
+        x_conv = self.conv(x_conv)[:, :, :L]
+        x_conv = x_conv.transpose(1,2)
+        x_act = self.activation(x_conv)
+        
+        x_and_ds = self.x_proj(x_act)
+        dt = self.dt_proj(x_act)
+        x, ds = x_and_ds.split([self.d_model, self.d_state], dim=-1)
+        dt = F.softplus(dt)
+        
+        # Selective scan
+        us = torch.zeros(B, L, self.d_state, device=x.device)
+        for i in range(L):
+            us[:, i] = us[:, i-1] if i > 0 else us[:, i]
+            # us[:, i] = us[:, i] * torch.exp(-dt[:, i]) + ds[:, i] * torch.exp(-dt[:, i] / 2)
+            us = us * torch.exp(-dt) + ds * torch.exp(-dt / 2)
+        
+        # Project us to match x's dimension
+        us_projected = self.us_proj(us)
+        
+        x = x + us_projected
+        
+        x = self.out_proj(torch.cat([x, x_act[:, :, self.d_model:]], dim=-1))
+        return x
+
+class Mamba(nn.Module):
+    def __init__(self, 
+        d_model: int, 
+        d_state: int, 
+        d_conv: int, 
+        expand: int, 
+        depth: int, 
+        n_classes=2
+    ):
+        """ Mamba
+        
+        Args: 
+            d_model (int): the input dimension.
+            d_state (int): the state dimension.
+            d_conv (int): the convolutional kernel size.
+            expand (int): the expansion factor.
+            depth (int): the depth of the model.
+            n_classes (int): the number of classes.
+
+        References: 
+            1. Gu, A., & Dao, T. (2023). 
+            Mamba: Linear-time sequence modeling with selective state spaces. 
+            arXiv preprint arXiv:2312.00752.
+        """
+        super().__init__()
+        self.layers = nn.ModuleList([MambaBlock(d_model, d_state, d_conv, expand) for _ in range(depth)])
+        self.fc = nn.Linear(d_model, n_classes)
+        
+    def forward(self, x):
+        """ Forward pass
+        
+        Args: 
+            x (torch.Tensor): the input tensor.
+
+        Returns: 
+            x (torch.Tensor): the output tensor.
+        """
+        # Add sequence dimenion
+        x = x.unsqueeze(1)
+        # Make the sequence length 100
+        x = x.repeat(1, 100, 1)
+        for layer in self.layers:
+            x = x + layer(x)
+        x = self.fc(x)
+        # Remove the sequence dimension
+        x = x[:, 0, :]
+        return x
