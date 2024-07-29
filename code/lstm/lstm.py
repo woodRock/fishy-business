d63add04a3e0b3d2aa804d22807def0ed024fbdd
woodRock <j.r.h.wood98@gmail.com>
2024-07-29 12:59:51 +1200

    Add references to code

--- code/lstm/lstm.py
+++ code/lstm/lstm.py
@@ -19,19 +19,36 @@ class LSTM(nn.Module):
             num_layers (int): the number of hidden layers. Defaults to 2.
 
         References: 
-            1. Hochreiter, S., & Schmidhuber, J. (1997). Long short-term memory. Neural computation, 9(8), 1735-1780.
+            1. Hochreiter, S., & Schmidhuber, J. (1997). 
+                Long short-term memory. 
+                Neural computation, 9(8), 1735-1780.
+            2. Srivastava, N., Hinton, G., Krizhevsky, A.,
+                Sutskever, I., & Salakhutdinov, R. (2014).
+                Dropout: a simple way to prevent neural networks from overfitting.
+                The journal of machine learning research, 15(1), 1929-1958.
+            3. Hinton, G. E., Srivastava, N., Krizhevsky, A., Sutskever,
+                I., & Salakhutdinov, R. R. (2012).
+                Improving neural networks by preventing co-adaptation of feature detectors.
+                arXiv preprint arXiv:1207.0580.
+            4. Loshchilov, I., & Hutter, F. (2017). 
+                Decoupled weight decay regularization. 
+                arXiv preprint arXiv:1711.05101.
+            5. Szegedy, C., Vanhoucke, V., Ioffe, S., Shlens, J., & Wojna, Z. (2016).
+                Rethinking the inception architecture for computer vision.
+                In Proceedings of the IEEE conference on computer vision
+                and pattern recognition (pp. 2818-2826).
         
         """
         super(LSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         
-        # LSTM layer
+        # LSTM layer (Hochreiter 1997)
         self.lstm = nn.LSTM(input_size=input_size, 
                             hidden_size=hidden_size,
                             num_layers=num_layers, 
                             batch_first=True)
-        
+        # Dropout layer (Srivastava 2014, Hinton 2012)
         self.dropout = nn.Dropout(p=dropout)
         
         # Fully connected layer
@@ -62,7 +79,7 @@ class LSTM(nn.Module):
         # Forward propagate LSTM
         out, _ = self.lstm(x, (h0, c0))  # out: tensor of shape (batch_size, seq_length, hidden_size)
 
-        # Dropout
+        # Dropout layer (Srivastava 2014, Hinton 2012)
         out = self.dropout(out)
         
         # Decode the hidden state of the last time step
